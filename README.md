a3922969400ad98acd0d1bee440376c6b745c1e5
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-04-15 19:15:08 -0300

    react-router-dom e rotas ola mundo e sobremim

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+<h3>Alura</h3>
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
+console.log(window.location);
 
 function App() {
   return (
-   <h1>Olá Mundo!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />}/>
+        <Route path="/sobremim" element={<SobreMim />}/>
+        <Route path="*" element={<div>Bad Request: Página não encontrada</div>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
