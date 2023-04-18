c040b31d8fd0c33142627d90546200e1a26735e6
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-04-18 11:01:20 -0300

    rotas aninhadas

---
+++ src/componentes/Post/index.js
@@ -0,0 +1,11 @@
+import styles from './Post.module.css';
+
+export default function Post ({post}) {
+    return (
+        <div className={styles.post}>
+            <img className={styles.capa} src={`../../posts/${post.id}/capa.png`} alt="capa"/>
+            <h2 className={styles.titulo}>{post.titulo}</h2>
+            <button className={styles.botaoLer}>Ler</button>
+        </div>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import Menu from "./componentes/Menu";
+import Rodape from "./componentes/Rodape";
 import Inicio from "./paginas/Inicio";
+import PaginaPadrao from "./paginas/PaginaPadrao";
 import SobreMim from "./paginas/SobreMim";
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
@@ -10,10 +12,13 @@ function AppRoutes() {
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path="/" element={<Inicio />}/>
-        <Route path="/sobremim" element={<SobreMim />}/>
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />}/>
+          <Route path="/sobremim" element={<SobreMim />}/>
+        </Route>
         <Route path="*" element={<div>Bad Request: Página não encontrada</div>}/>
       </Routes>
+      <Rodape />
     </BrowserRouter>
   );
 }
