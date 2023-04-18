3a3f0a9e2f9552423d8f4bbc902e32236217058c
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-04-18 20:45:25 -0300

    utilizando parametros nas rotas

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import PaginaPadrao from "./paginas/PaginaPadrao";
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Inicio from "../src/paginas/Inicio";
 import SobreMim from "../src/paginas/SobreMim";
+import Post from "./paginas/Post";
 
 console.log(window.location);
 
@@ -15,6 +16,7 @@ function AppRoutes() {
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />}/>
           <Route path="sobremim" element={<SobreMim />}/>
+          <Route path="posts/:id" element={<Post />}></Route>
         </Route>
         <Route path="*" element={<div>Bad Request: Página não encontrada</div>}/>
       </Routes>
