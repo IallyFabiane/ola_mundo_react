06b6ca4082e83e631bf3b2f562f5a7fd184606f0
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-04-19 16:06:38 -0300

    componente ScrollToTop e posts recomendados

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,21 +1,47 @@
-import { useParams } from "react-router-dom";
+import { useParams, Routes, Route } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 import posts from '../../json/posts.json';
 import PostModelo from "../../componentes/PostModelo";
 import './Post.css';
+import NaoEncontrado from "../NaoEncontrado";
+import PaginaPadrao from "../PaginaPadrao";
+import styles from './Post.module.css';
+import PostCard from "../../componentes/PostCard";
 
 export default function Post () {
+
     const parametros = useParams();
     const post = posts.find((post) => post.id === Number(parametros.id))
-    if(!post) {
-        return <h1>Post não encontrado...</h1>
+    
+    if (!post) {
+        return <NaoEncontrado />
     }
     
+    const postsRecomendados = posts
+    .filter((post) => (post.id !== Number(parametros.id)))
+    .sort((a, b) => b.id - a.id)
+    .slice(0 , 4)
+
     return (
-        <PostModelo fotoCapa={`/posts/${post.id}/capa.png`} titulo={post.titulo}>
-           <div className="post-markdown-container">
-             <ReactMarkdown>{post.texto}</ReactMarkdown> 
-          </div>
-        </PostModelo>
+        <Routes>
+            <Route path="*" element={<PaginaPadrao />}>
+                <Route index  element={
+                    <PostModelo fotoCapa={`/posts/${post.id}/capa.png`} titulo={post.titulo}>
+                        <div className="post-markdown-container">
+                            <ReactMarkdown>{post.texto}</ReactMarkdown> 
+                        </div>
+
+                        <h2 className={styles.titulosOutrosPosts}>Outros posts que você pode gostar:</h2>
+                        <ul className={styles.postsRecomendados}>{postsRecomendados.map((post) => {
+                            return (
+                            <li key={post.id}>
+                                <PostCard post={post} />
+                            </li>
+                            )
+                        })}</ul>
+                    </PostModelo>
+                }/>
+            </Route>
+        </Routes>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -6,19 +6,21 @@ import Inicio from "../src/paginas/Inicio";
 import SobreMim from "../src/paginas/SobreMim";
 import Post from "./paginas/Post";
 import NaoEncontrado from "./paginas/NaoEncontrado";
+import ScrollToTop from "./componentes/ScrollToTop";
 
 console.log(window.location);
 
 function AppRoutes() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <Menu />
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />}/>
           <Route path="sobremim" element={<SobreMim />}/>
-          <Route path="posts/:id" element={<Post />}></Route>
         </Route>
+        <Route path="posts/:id/*" element={<Post />}/>
         <Route path="*" element={<NaoEncontrado />}/>
       </Routes>
       <Rodape />
